b361a4f74f78621d3dae976ae58fec35ad413eb6
07602520 <raymon3838@gmail.com>
2017-04-10 16:51:37 +0800

    Add files via upload

---
+++ _05-1.ino
@@ -0,0 +1,29 @@
+#include <Keypad.h> 
+
+#define KEY_ROWS 4 
+#define KEY_COLS 4 
+ 
+
+char keymap[KEY_ROWS][KEY_COLS] = {
+  {'0', '4', '8', 'C'},
+  {'1', '5', '9', 'D'},
+  {'2', '6', 'A', 'E'},
+  {'3', '7', 'B', 'F'}};
+  
+byte colPins[KEY_COLS] = {9,8,7,6};	   。
+byte rowPins[KEY_ROWS] = {5,4,3,2};  
+
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, KEY_ROWS, KEY_COLS);
+ 
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  
+  char key = myKeypad.getKey();
+  
+  if (key){  
+    Serial.println(key);  
+  }
+}
---
+++ _05-2.ino
@@ -0,0 +1,55 @@
+#include <Keypad.h> 
+#include <SevSeg.h>
+
+SevSeg sevseg;
+
+#define KEY_ROWS 4 
+#define KEY_COLS 4 
+
+
+char keymap[KEY_ROWS][KEY_COLS] = {
+  {'0', '4', '8', 'C'},
+  {'1', '5', '9', 'D'},
+  {'2', '6', 'A', 'E'},
+  {'3', '7', 'B', 'F'}};
+  
+byte colPins[KEY_COLS] = {9,8,7,6};	   。
+byte rowPins[KEY_ROWS] = {5,4,3,2}; 
+ 
+
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, KEY_ROWS, KEY_COLS);
+
+void setup()
+{
+  Serial.begin(9600);
+  byte numDigits = 1;
+  byte digitPins[] = {10};
+  byte segmentPins[] = {A0,A1,A4,A3,A2,12,13,A5};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(90);
+}
+  
+void loop()
+{
+  
+  char key = myKeypad.getKey();
+  char i,sum,sux=0;
+  if (key != NO_KEY)
+  {
+    Serial.println(key);
+    for(i=48;i<58;i++)
+    {
+      if(key == i)
+      {
+        sevseg.setNumber(i-48,2);
+      }
+    }
+    for (i=65;i<71;i++)
+    {
+      if(key == i)
+        sevseg.setNumber(i-55,2);
+    }
+  }
+  sevseg.refreshDisplay();
+
+}
